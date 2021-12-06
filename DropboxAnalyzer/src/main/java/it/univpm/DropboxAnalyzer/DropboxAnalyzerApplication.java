4602f54c8de03e8e0cc90fee4f5d14a3bb1c7f16
francescocecca <cecca.francesco@yahoo.it>
2021-12-06 16:29:39 +0100

    Creazione Progetto

---
+++ DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/DropboxAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.DropboxAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropboxAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropboxAnalyzerApplication.class, args);
+	}
+
+}
---
+++ DropboxAnalyzer/src/test/java/it/univpm/DropboxAnalyzer/DropboxAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.DropboxAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropboxAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
