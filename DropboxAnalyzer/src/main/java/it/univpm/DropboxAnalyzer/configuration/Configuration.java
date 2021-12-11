4b2d496897b264e5385dc6cd0c833f334ee30b80
lorenzobartolini00 <s1092626@studenti.univpm.it>
2021-12-11 17:12:17 +0100

    Modifica di HTTPRequest class

--- DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/configuration/Configuration.java
+++ DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/configuration/Configuration.java
@@ -3,10 +3,12 @@ package it.univpm.DropboxAnalyzer.configuration;
 public class Configuration {
 	private String url;
 	private Body body;
+	private String type;
 	private String token;
-	public Configuration(String url, Body body, String token) {
+	public Configuration(String url, Body body, String type, String token) {
 		this.url = url;
 		this.body = body;
+		this.type = type;
 		this.token = token;
 	}
 	public String getUrl() {
@@ -20,6 +22,12 @@ public class Configuration {
 	}
 	public void setBody(Body body) {
 		this.body = body;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
 	}
 	public String getToken() {
 		return token;
