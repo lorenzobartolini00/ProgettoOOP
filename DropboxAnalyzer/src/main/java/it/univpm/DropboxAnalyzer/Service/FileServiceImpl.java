b93b83c703af4282e28f1c8954780ef310aab96d
lorenzobartolini00 <s1092626@studenti.univpm.it>
2021-12-12 11:32:32 +0100

    Specializzazione User in Editor e Owner + fix getUserLIst()

--- DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/Service/FileServiceImpl.java
+++ DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/Service/FileServiceImpl.java
@@ -21,19 +21,19 @@ public class FileServiceImpl implements FileService {
 	HTTPSRequest httpsReq;
 	
 	@Override
-	public Vector<Revision> getListRevisions(JSONObject JSONrevisions) {
+	public Vector<Revision> getRevisionList(JSONObject jsonObjectRevisions) {
 		Vector<Revision> revisionList = new Vector<Revision>();
 		
 		//Ottengo il jsonArray che contiene la lista delle revisioni per il file di interesse
-        JSONArray jsonArray= (JSONArray) JSONrevisions.get("entries");
+        JSONArray jsonArrayRevisions = (JSONArray) jsonObjectRevisions.get("entries");
         
         //Per ogni JSONObject dentro il jsonArray estraggo i 3 valori che mi interessano e li uso per inizializzare una nuova
         //istanza della classe Revision
-        for(Object jsonObject : jsonArray)
+        for(Object jsonObjectRevision : jsonArrayRevisions)
         {
-        	String lastClientModify = ((JSONObject) jsonObject).getString("client_modified");
-        	String lastServerModify = ((JSONObject) jsonObject).getString("server_modified");
-        	String revisionId = ((JSONObject) jsonObject).getString("rev");
+        	String lastClientModify = ((JSONObject) jsonObjectRevision).getString("client_modified");
+        	String lastServerModify = ((JSONObject) jsonObjectRevision).getString("server_modified");
+        	String revisionId = ((JSONObject) jsonObjectRevision).getString("rev");
 
         	Revision revision = new Revision(lastClientModify, lastServerModify, revisionId);
         	
@@ -45,56 +45,58 @@ public class FileServiceImpl implements FileService {
 		}
 
 	@Override
-	public Vector<Content> getListFolder(JSONObject jsonObj) {
-		Vector<Content> listFolder = new Vector<Content>();
+	public Vector<Content> getContentList(JSONObject jsonObjectFolders) {
+		Vector<Content> contentList = new Vector<Content>();
 		
 		//Ottengo il jsonArray che contiene la lista delle revisioni per il file di interesse
-        JSONArray jsonArray= (JSONArray) jsonObj.get("entries");
+        JSONArray jsonArrayFolders = (JSONArray) jsonObjectFolders.get("entries");
         
-        for(Object jsonObject : jsonArray) {
-        	String name=((JSONObject) jsonObject).getString("name");
-        	String pathLower=((JSONObject) jsonObject).getString("path_lower");
-        	String pathDisplay=((JSONObject) jsonObject).getString("path_display");
-        	String id=((JSONObject) jsonObject).getString("id");
+        for(Object jsonObjectContent : jsonArrayFolders) {
+        	String name=((JSONObject) jsonObjectContent).getString("name");
+        	String pathLower=((JSONObject) jsonObjectContent).getString("path_lower");
+        	String pathDisplay=((JSONObject) jsonObjectContent).getString("path_display");
+        	String id=((JSONObject) jsonObjectContent).getString("id");
         	
         	Content content = null;
-        	if(((JSONObject) jsonObject).getString(".tag").equals("file"))
+        	
+        	//In base all'attributo tag, decido se si tratta di un File o un Folder
+        	if(((JSONObject) jsonObjectContent).getString(".tag").equals("file"))
         	{
         		//TODO: Gestire eccezioni
-        		Long size = Long.valueOf( ((JSONObject) jsonObject).getString("size") );
-        		boolean isDownloadable = Boolean.parseBoolean(((JSONObject) jsonObject).getString("is_downloadable"));
+        		Long size = Long.valueOf( ((JSONObject) jsonObjectContent).getString("size") );
+        		boolean isDownloadable = Boolean.parseBoolean(((JSONObject) jsonObjectContent).getString("is_downloadable"));
         		content = new File(name, pathLower, pathDisplay, id, size, isDownloadable);
         	}
-        	else if(((JSONObject) jsonObject).getString(".tag").equals("folder"))
+        	else if(((JSONObject) jsonObjectContent).getString(".tag").equals("folder"))
         	{
-        		content = new Folder(name,pathLower,pathDisplay,id);
+        		content = new Folder(name, pathLower, pathDisplay, id);
         	}
         	
-        	if(content != null) listFolder.add(content);
+        	if(content != null) contentList.add(content);
         }
         
-		return listFolder;
+		return contentList;
 	}
 
 	@Override
-	public Content getMetadata(JSONObject JSONContent) {
+	public Content getMetadata(JSONObject jsonObjectContent) {
 		
-		String name=((JSONObject) JSONContent).getString("name");
-		String pathLower=((JSONObject) JSONContent).getString("path_lower");
-    	String pathDisplay=((JSONObject) JSONContent).getString("path_display");
-    	String id=((JSONObject) JSONContent).getString("id");
+		String name=((JSONObject) jsonObjectContent).getString("name");
+		String pathLower=((JSONObject) jsonObjectContent).getString("path_lower");
+    	String pathDisplay=((JSONObject) jsonObjectContent).getString("path_display");
+    	String id=((JSONObject) jsonObjectContent).getString("id");
     	
     	Content content = null;
-    	if(((JSONObject) JSONContent).getString(".tag").equals("file"))
+    	if(((JSONObject) jsonObjectContent).getString(".tag").equals("file"))
     	{
     		//TODO: Gestire eccezioni
-    		Long size = Long.valueOf( ((JSONObject) JSONContent).getString("size") );
-    		boolean isDownloadable = Boolean.parseBoolean(((JSONObject) JSONContent).getString("is_downloadable"));
+    		Long size = Long.valueOf( ((JSONObject) jsonObjectContent).getString("size") );
+    		boolean isDownloadable = Boolean.parseBoolean(((JSONObject) jsonObjectContent).getString("is_downloadable"));
     		content = new File(name, pathLower, pathDisplay, id, size, isDownloadable);
     	}
-    	else if(((JSONObject) JSONContent).getString(".tag").equals("folder"))
+    	else if(((JSONObject) jsonObjectContent).getString(".tag").equals("folder"))
     	{
-    		content = new Folder(name,pathLower,pathDisplay,id);
+    		content = new Folder(name, pathLower, pathDisplay, id);
     	}
     	
     	return content;
--- DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/Service/UserService.java
+++ DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/Service/UserService.java
@@ -2,10 +2,12 @@ package it.univpm.DropboxAnalyzer.Service;
 
 import java.util.*;
 import org.json.JSONObject;
+import org.springframework.stereotype.Service;
 
 import it.univpm.DropboxAnalyzer.Model.*;
 
+@Service
 public interface UserService {
 
-	public Vector<User> getListUsers(JSONObject jsonObj);
+	public Vector<User> getUserList(JSONObject jsonObj);
 }
