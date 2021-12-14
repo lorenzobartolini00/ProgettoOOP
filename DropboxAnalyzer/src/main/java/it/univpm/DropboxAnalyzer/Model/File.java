ca98b4929b14d2b9dd3dc977f50764f882144473
lorenzobartolini00 <s1092626@studenti.univpm.it>
2021-12-14 18:07:43 +0100

    Aggiunto attributo "extension" nella classe File

--- DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/Model/File.java
+++ DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/Model/File.java
@@ -5,12 +5,19 @@ public class File extends Content{
 	//attributi
 	private Long size;
 	private Boolean isDownloadable;
+	private String extension;
 	
 	//costruttore
 	public File(String name, String pathLower, String pathDisplay, String id, Long size, Boolean isDownloadable) {
 		super(name, pathLower, pathDisplay, id);
 		this.size = size;
 		this.isDownloadable = isDownloadable;
+		this.extension = toExtension(pathLower);
+	}
+
+	private String toExtension(String pathLower) {
+		String[] strings = pathLower.split(".");
+		return "." + strings[strings.length - 1];
 	}
 
 	//getter e setter
@@ -29,6 +36,15 @@ public class File extends Content{
 	public void setIsDownloadable(Boolean isDownloadable) {
 		this.isDownloadable = isDownloadable;
 	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+	
 	
 	
 }
