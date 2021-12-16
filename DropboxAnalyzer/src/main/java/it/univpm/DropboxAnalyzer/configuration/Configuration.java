709d59d9724dc98bccf04c5c634770f510e74245
lorenzobartolini00 <s1092626@studenti.univpm.it>
2021-12-16 17:14:35 +0100

    Aggiunta interfaccia Configuration e sottoclassi specifiche per ogni rotta
    La classe Configuration è diventata un'interfaccia che prevede un metodo setDefault(), il quale si occupa di settare i parametri di default per la chiamata http(quali l'url, il tipo della richiesta, il token più i parametri specifici per ciascuna API)

--- DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/configuration/Configuration.java
+++ DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/configuration/Configuration.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.json.JSONObject;
 
+import it.univpm.DropboxAnalyzer.Service.BadFormatException;
+
 public interface Configuration {
-	public void setDefault(Map<String, Object> parameters);
+	public void setDefault(Map<String, Object> parameters) throws BadFormatException;
 }
