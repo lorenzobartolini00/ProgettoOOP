5e1c0b5abbe351de3e98fe949b1913fc248fe8c3
lorenzobartolini00 <s1092626@studenti.univpm.it>
2021-12-17 21:07:24 +0100

    Aggiunti filtri minSize e maxSize in FileFilter e RevisionFilter

--- DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/filter/FileFilter.java
+++ DropboxAnalyzer/src/main/java/it/univpm/DropboxAnalyzer/filter/FileFilter.java
@@ -14,6 +14,8 @@ import it.univpm.DropboxAnalyzer.configuration.Configuration;
 public class FileFilter implements Filter{
 	
 	private String fileExtension;
+	private Long minSize;
+	private Long maxSize;
 	private boolean onlyDownloadable;
 
 	private Vector<Content> contents;
@@ -30,6 +32,8 @@ public class FileFilter implements Filter{
 		//rimuovo prima tutti gli elementi di contents che non sono istanza
 		//della classe File
 		contents.removeIf(p -> !(p instanceof File));
+		if(maxSize != null) contents.removeIf(aboveThreshold());
+		if(minSize != null) contents.removeIf(belowThreshold());
 		if(fileExtension != null) contents.removeIf(notRightExtension());
 		if(onlyDownloadable != false) contents.removeIf(isNotDownloadable());
 	}
@@ -40,9 +44,9 @@ public class FileFilter implements Filter{
 		{
 			Map<String, Object> filters = (Map<String, Object>) parameters.get("filters");
 			
-			if(filters.containsKey("only_type"))
+			if(filters.containsKey("file_extensions"))
 			{
-				this.setFileExtension((String)filters.get("only_type"));
+				this.setFileExtension((String)filters.get("file_extensions"));
 			}
 			
 			if(filters.containsKey("only_downloadable"))
@@ -52,6 +56,14 @@ public class FileFilter implements Filter{
 			else
 			{
 				this.setOnlyDownloadable(false);
+			}
+			if(filters.containsKey("min_size"))
+			{
+				this.setMinSize(Integer.toUnsignedLong((Integer)filters.get("min_size"))) ;
+			}
+			if(filters.containsKey("max_size"))
+			{
+				this.setMaxSize(Integer.toUnsignedLong((Integer)filters.get("max_size"))) ;
 			}
 		}
 		
@@ -68,6 +80,18 @@ public class FileFilter implements Filter{
 		return p -> (!((File) p).getIsDownloadable());
 	}
 	
+	private Predicate<Content> aboveThreshold() {
+		//L'elemento viene rimosso se se la seguente condizione è verificata,
+		//ovvero se la dimensione dell'elemento è maggiore alla soglia
+        return p -> (((File) p).getSize() > maxSize);
+    }
+	
+	private Predicate<Content> belowThreshold() {
+		//L'elemento viene rimosso se se la seguente condizione è verificata,
+		//ovvero se la dimensione dell'elemento è minore o uguale alla soglia
+        return p -> (((File) p).getSize() <= minSize);
+    }
+	
 	//getter e setter
 	public String getFileExtension() {
 		return fileExtension;
@@ -83,6 +107,22 @@ public class FileFilter implements Filter{
 
 	public void setOnlyDownloadable(boolean onlyDownloadable) {
 		this.onlyDownloadable = onlyDownloadable;
+	}
+
+	public long getMinSize() {
+		return minSize;
+	}
+
+	public void setMinSize(long minSize) {
+		this.minSize = minSize;
+	}
+
+	public long getMaxSize() {
+		return maxSize;
+	}
+
+	public void setMaxSize(long maxSize) {
+		this.maxSize = maxSize;
 	}
 
 	public Vector<Content> getContents() {
